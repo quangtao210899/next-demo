fe5cfb25f6c9fc26e6791544e0f7fba6837afbc2
quangtao210899 <quangtaobaongoc@gmail.com>
2024-04-26 15:20:59 +0700

    update

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -13,16 +13,21 @@ import MuiCard, { CardProps } from '@mui/material/Card'
 import InputAdornment from '@mui/material/InputAdornment'
 import FormLabel from '@mui/material/FormLabel';
 import Grid from '@mui/material/Grid';
-import EyeOutline from 'mdi-material-ui/EyeOutline'
-import EyeOffOutline from 'mdi-material-ui/EyeOffOutline'
+import Paper from '@mui/material/Paper';
 
-const Card = styled(MuiCard)<CardProps>(({ theme }) => ({
-  [theme.breakpoints.up('sm')]: { width: '28rem' }
-}))
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 const portal = () => {
   return (
     <Box className='content-center'>
-      <Box sx={{mt: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      {/* C3P */}
+      <Box sx={{ mt: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <Typography
           sx={{
             fontSize: '96px !important',
@@ -35,10 +40,41 @@ const portal = () => {
           C3P
         </Typography>
       </Box>
-      <Card sx={{ zIndex: 1 }}>
-        {/* <CardContent sx={{ padding: theme => `${theme.spacing(3, 5, 5)} !important` }}>
-        </CardContent> */}
-      </Card>
+      {/* Text random */}
+      <Box sx={{ mt: 0,mb: 5, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Typography
+          sx={{
+            fontSize: '36px !important',
+            fontWeight: '700 !important',
+            color: "#5C5C5C",
+            lineHieght: "14.06px",
+            fontFamily: "Roboto"
+          }}
+        >
+          企業名
+        </Typography>
+      </Box>
+
+      <Grid container justifyContent="space-between"  spacing={{ xs: 8, md: 12 }} columns={{ xs: 4, sm: 8, md: 12 }} style={{ padding: '0 120px' }}>
+        <Grid item xs={2} sm={4} md={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Item>xs=4</Item>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
