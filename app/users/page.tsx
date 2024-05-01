00a5edbc1967d4a2a88cde9896b1acb879e968df
unknown <quangtaobaongoc@gmail.com>
2024-05-01 10:18:17 +0700

    update

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,8 @@
 import * as React from 'react';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
 import Table from '@mui/material/Table';
 import Grid from '@mui/material/Grid';
 import TableBody from '@mui/material/TableBody';
@@ -20,11 +22,10 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 interface Data {
   id: number;
-  calories: number;
-  carbs: number;
-  fat: number;
   name: string;
-  protein: number;
+  calories: number;
+  fat: number;
+  chip: Array<string>;
 }
 
 function createData(
@@ -32,33 +33,31 @@ function createData(
   name: string,
   calories: number,
   fat: number,
-  carbs: number,
-  protein: number,
+  chip: Array<string>,
 ): Data {
   return {
     id,
     name,
     calories,
     fat,
-    carbs,
-    protein,
+    chip,
   };
 }
 
 const rows = [
-  createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-  createData(2, 'Donut', 452, 25.0, 51, 4.9),
-  createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-  createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-  createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
-  createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-  createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-  createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-  createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-  createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-  createData(13, 'Oreo', 437, 18.0, 63, 4.0),
+  createData(1, 'Cupcake', 305, 3.7, ["user1", "user2"]),
+  createData(2, 'Donut', 452, 25.0, ["user1", "user2"]),
+  createData(3, 'Eclair', 262, 16.0, ["user1", "user2"]),
+  createData(4, 'Frozen yoghurt', 159, 6.0, ["user1", "user2"]),
+  createData(5, 'Gingerbread', 356, 16.0, ["user1", "user2"]),
+  createData(6, 'Honeycomb', 408, 3.2, ["user1", "user2"]),
+  createData(7, 'Ice cream sandwich', 237, 9.0, ["user1", "user2"]),
+  createData(8, 'Jelly Bean', 375, 0.0, ["user1", "user2"]),
+  createData(9, 'KitKat', 518, 26.0, ["user1", "user2"]),
+  createData(10, 'Lollipop', 392, 0.2, ["user1", "user2"]),
+  createData(11, 'Marshmallow', 318, 0, ["user1", "user2"]),
+  createData(12, 'Nougat', 360, 19.0, ["user1", "user2"]),
+  createData(13, 'Oreo', 437, 18.0, ["user1", "user2"]),
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -77,8 +76,8 @@ function getComparator<Key extends keyof any>(
   order: Order,
   orderBy: Key,
 ): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string },
+  a: { [key in Key]: number | string | Array<string> },
+  b: { [key in Key]: number | string | Array<string>},
 ) => number {
   return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
@@ -124,17 +123,11 @@ const headCells: readonly HeadCell[] = [
     label: 'Fat (g)',
   },
   {
-    id: 'carbs',
-    numeric: true,
+    id: 'chip',
+    numeric: false,
     disablePadding: false,
-    label: 'Carbs (g)',
-  },
-  {
-    id: 'protein',
-    numeric: true,
-    disablePadding: false,
-    label: 'Protein (g)',
-  },
+    label: 'Chip',
+  }
 ];
 
 interface EnhancedTableProps {
@@ -269,8 +262,7 @@ export default function EnhancedTable() {
   const isSelected = (id: number) => selected.indexOf(id) !== -1;
 
   // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   const visibleRows = React.useMemo(
     () =>
@@ -353,8 +345,15 @@ export default function EnhancedTable() {
                     </TableCell>
                     <TableCell align="right">{row.calories}</TableCell>
                     <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell align="left">
+                      <Stack direction="row" spacing={1}>
+                        {row.chip.map((chip_data, chip_key) => {
+                          return (
+                            <Chip label={chip_data} color="primary" />
+                          )
+                        })}
+                      </Stack>
+                      </TableCell>
                   </TableRow>
                 );
               })}
