ad234f58cedd0fb4bb349d79d010d38e048f7dd9
quangtao210899 <quangtaobaongoc@gmail.com>
2024-05-06 10:03:57 +0700

    update

--- app/targets/detail/[id]/page.tsx
+++ app/targets/detail/[id]/page.tsx
@@ -53,88 +53,88 @@ const detailTarget = ({ params }: { params: { id: number } }) => {
       </Grid>
       <Grid container direction="row" justifyContent="space-around" alignItems="stretch" spacing={{ xs: 1, sm: 3, md: 4 }} columns={{ xs: 12, sm: 12, md: 12 }}>
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px' }}>
+          <Typography sx={{ fontSize: '20px', ml: 2 }}>
             対象名
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             Target
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={12} md={12}>
-          <Divider sx={{ borderColor: "#68A7B9", borderWidth: '1px' }} />
+          <Divider sx={{ borderColor: "#68A7B9", borderWidth: '1px', ml: 2 }} />
         </Grid>
 
         <Grid item xs={12} sm={12} md={12}>
-          <Typography sx={{ fontSize: '20px' }}>
+          <Typography sx={{ fontSize: '20px', ml: 2 }}>
             対象属性
           </Typography>
         </Grid>
         {/* sub items */}
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 } }}>
+          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 }, ml: 3 }}>
             性別
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             男性
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 } }}>
+          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 }, ml: 3 }}>
             年代
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             40代
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 } }}>
+          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 }, ml: 3 }}>
             地域
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             東京都
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 } }}>
+          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 }, ml: 3 }}>
             時間帯
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             男性
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 } }}>
+          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 }, ml: 3 }}>
             家族構成
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             既婚－子ども有
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={3} md={3}>
-          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 } }}>
+          <Typography sx={{ fontSize: '20px', [theme.breakpoints.up('sm')]: { ml: 5 }, ml: 3 }}>
             職業
           </Typography>
         </Grid>
         <Grid item xs={12} sm={9} md={9}>
-          <Typography sx={{ fontSize: '20px', color: "#646464" }}>
+          <Typography sx={{ fontSize: '20px', color: "#646464", [theme.breakpoints.up('sm')]: { ml: 0 }, ml: 3 }}>
             エンジニア
           </Typography>
         </Grid>
